b5aca00d3bbebea2ae64f16b84ce5a73570d57cc
Grisha596 <rijalgrishma5@gmail.com>
2023-10-17 14:08:19 +0545

    Adding changes to games

---
+++ linefollowing.ino
@@ -0,0 +1,15 @@
+//motor
+int leftmotorpin1 = 12;
+int leftmotorpin2 = 13;
+int rightmotorpin1 = 10;
+int rightmotorpin2 = 8;
+
+//sensor pin
+int leftsensorpin = 5;
+int midsensorpin = 6;
+int rightsensorpin = 7;
+
+//sensor state
+int leftsensorstate;
+int midsensorstate;
+int rightsensorstate;
