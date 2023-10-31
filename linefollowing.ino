87342efabbae2d054289e8d89d13c8a5242634d4
Grisha596 <rijalgrishma5@gmail.com>
2023-10-31 09:51:03 +0545

    Added code for audrino

--- linefollowing.ino
+++ linefollowing.ino
@@ -28,3 +28,57 @@ void setup() {
   Serial.begin(9600);
 
 }
+void loop() {
+  // put your main code here, to run repeatedly:
+  leftsensorstate = digitalRead(leftsensorpin);
+  midsensorstate = digitalRead(midsensorpin);
+  rightsensorstate = digitalRead(rightsensorpin);
+  
+  int result = (leftsensorstate<<2|midsensorstate<<1|rightsensorstate<<0);
+//  Serial.println(result, BIN);
+
+  if(result == 0b010){
+    
+    digitalWrite(leftmotorpin1, HIGH);
+    digitalWrite(leftmotorpin2, LOW);
+    digitalWrite(rightmotorpin1, HIGH);
+    digitalWrite(rightmotorpin2, LOW);
+
+    
+  }
+  else if(result == 0b011 || result == 0b001){
+    digitalWrite(leftmotorpin1, HIGH);
+    digitalWrite(leftmotorpin2, LOW);
+    digitalWrite(rightmotorpin1, LOW);
+    digitalWrite(rightmotorpin2, LOW);
+
+    delay(50);
+  }
+  else if(result == 0b110 || result == 0b100){
+    digitalWrite(leftmotorpin1, LOW);
+    digitalWrite(leftmotorpin2, LOW);
+    digitalWrite(rightmotorpin1, HIGH);
+    digitalWrite(rightmotorpin2, LOW);
+    
+    delay(50);
+  }
+//  else if(result == 0b000){
+//    digitalWrite(leftmotorpin1, LOW);
+//    digitalWrite(leftmotorpin2, LOW);
+//    digitalWrite(rightmotorpin1, HIGH);
+//    digitalWrite(rightmotorpin2, LOW);
+//  }
+
+  else if (result == 0b111){
+      digitalWrite(leftmotorpin1, HIGH);
+      digitalWrite(leftmotorpin2, LOW);
+      digitalWrite(rightmotorpin1, HIGH);
+      digitalWrite(rightmotorpin2, LOW);
+      delay (500);
+      digitalWrite(leftmotorpin1, LOW);
+      digitalWrite(leftmotorpin2, LOW);
+      digitalWrite(rightmotorpin1, LOW);
+      digitalWrite(rightmotorpin2, LOW);
+  }
+  
+}
